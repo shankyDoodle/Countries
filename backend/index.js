121725cf2434d8be0be614807e7d894ce54f2a56
skaldat1 <skaldat1@binghamton.edu>
2021-06-02 11:28:37 -0700

    init Backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const express = require('express')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const OK = 200;
+const CREATED = 201;
+const BAD_REQUEST = 400;
+const NOT_FOUND = 404;
+const CONFLICT = 409;
+const SERVER_ERROR = 500;
+
+const port = 9090;
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+app.use(bodyParser.json({limit: '10mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
+
+app.get('/', (req, res) => res.send("hello"));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
